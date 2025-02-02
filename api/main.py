7aa923d9b27b193660f760ffff067e53fc21d93a
abdp648 <138868148+abdp648@users.noreply.github.com>
2025-02-02 21:39:48 +0200

    GymFlowAPI

--- api/main.py
+++ api/main.py
@@ -2,14 +2,22 @@ from fastapi import FastAPI, Query
 from pymongo import MongoClient
 import os
 
-myclient = pymongo.MongoClient('mongodb+srv://abdoeisaebrahim2011:abdoeisa2011@cluster0.9yxwg.mongodb.net/')
-db = myclient['GymFlow']
-col = db['Exercises']
-
 app = FastAPI()
+
+# Load MongoDB URI from environment variables
+MONGO_URI = os.getenv("MONGO_URI")
+if not MONGO_URI:
+    raise ValueError("MONGO_URI is not set! Add it in Vercel environment variables.")
+
+# Connect to MongoDB
+client = MongoClient(MONGO_URI)
+db = client["GymFlow"]
+col = db["Exercises"]
+
 @app.get("/")
 def greet():
-    return "Hello bro it's working"
+    return {"message": "Hello bro, it's working!"}
+
 @app.post("/calculate_bmi")
 def calculate_bmi(weight: float, height: float, age: int, gender: str):
     bmi = weight / (height ** 2)
@@ -26,23 +34,14 @@ def calculate_bmi(weight: float, height: float, age: int, gender: str):
         "body_fat_percentage": round(body_fat, 2)
     }
 
-@app.get("/find_Exercize")
-def find_Exercize(name: str = Query(...)):
-    query = {"Name" : name}
-    results = col.find(query)
-    result_list = [
-        {"Title": doc.get("Title"), 
-        "Name": doc.get("Name"), 
-        "Muscle": doc.get("Muscle"),
-        "Tool": doc.get("Tool"),
-        "VideoId": doc.get("VideoId")} 
-        for doc in results]
+@app.get("/find_exercise")
+def find_exercise(name: str):
+    query = {"Name": name}
+    results = col.find(query, {"_id": 0})
     
-    return result_list
+    return list(results)
 
-@app.get("/get_Cards")
-def get_Cards():
-    result = col.find({}, {"_id": 0, "Title": 1 , "Muscle": 1})  
-    titles_and_muscles = [{"Title": doc["Title"], "Muscle": doc["Muscle"]} for doc in result]
-    
-    return titles_and_muscles
+@app.get("/get_cards")
+def get_cards():
+    result = col.find({}, {"_id": 0, "Title": 1, "Muscle": 1})  
+    return list(result)
